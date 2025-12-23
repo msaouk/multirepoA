9ed56e5446a0eb96c3da9fb64210bd0d36b9bcb8
msaouk <33035493+msaouk@users.noreply.github.com>
2025-12-23 14:20:18 +0200

    monorepo-prep-v2 added wui

---
+++ eab-wui/Controller.java
@@ -0,0 +1,2 @@
+test
+RUN curl -O "https://access.cdn.redhat.com/content/origin/files/sha256/74/74e932a060b481ab9c4967ef5da942a5d7c2b739a003aff1aa5dc851271dbe89/openshift-client-linux-arm64-rhel9-4.18.4.tar.gz?user=a7c840dc26e942963754f8e469221ad6&_auth_=1741805241_2f903226305b707beca418f7019a7bbb"
